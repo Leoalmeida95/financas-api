8a6d4827d6a58723c5d322262c11736c0238ce4e
Leonardo Almeida <leo.almeida.silva@hotmail.com>
2020-04-08 19:17:29 -0300

    injeção de dependencia e exceptions

--- src/main/java/com/leo/minhasfinancas/model/repositoys/UsuarioRepository.java
+++ src/main/java/com/leo/minhasfinancas/model/repositoys/UsuarioRepository.java
@@ -5,5 +5,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.leo.minhasfinancas.model.entity.Usuario;
 
 public interface UsuarioRepository extends JpaRepository<Usuario,Long>{
-
+	
+	boolean existsByEmail(String email);
 }
---
+++ src/main/java/com/leo/minhasfinancas/service/UsuarioService.java
@@ -0,0 +1,11 @@
+package com.leo.minhasfinancas.service;
+
+import com.leo.minhasfinancas.model.entity.Usuario;
+
+public interface UsuarioService {
+
+		Usuario autenticar(String email, String senha);
+		Usuario salvarUsuario(Usuario usuario);
+		void validarEmail(String email);
+		
+}
