fc89a76dd95069d14c90067de59c2d5cef32dac3
Leonardo Almeida <leo.almeida.silva@hotmail.com>
2020-04-09 14:42:12 -0300

    teste de autenticacao

--- src/test/java/com/leo/minhasfinancas/service/UsuarioServiceTest.java
+++ src/test/java/com/leo/minhasfinancas/service/UsuarioServiceTest.java
@@ -1,13 +1,18 @@
 package com.leo.minhasfinancas.service;
 
+import java.util.Optional;
+
+import org.assertj.core.api.Assertions;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mockito;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import com.leo.minhasfinancas.exceptions.RegraNegocioException;
+import com.leo.minhasfinancas.model.entity.Usuario;
 import com.leo.minhasfinancas.model.repositorys.UsuarioRepository;
 import com.leo.minhasfinancas.service.impl.UsuarioServiceImpl;
 
@@ -19,13 +24,34 @@ public class UsuarioServiceTest {
 
 	
 	UsuarioService service;
+	
+	@MockBean
 	UsuarioRepository repository;
 	
 	@Before
 	public void setUp()
 	{
-		repository = Mockito.mock(UsuarioRepository.class);
 		service = new UsuarioServiceImpl(repository);
+	}
+	
+	@Test(expected = Test.None.class)
+	public void deveAutenticarUmUsuarioComSucesso() {
+		//cenario
+		String email = "leo@email.com";
+		String senha = "123";
+		
+		Usuario usuario = Usuario.builder()
+				.email(email)
+				.senha(senha)
+				.build();
+		
+		Mockito.when(repository.findByEmail(email)).thenReturn(Optional.of(usuario));
+		
+		//acao
+		Usuario result = service.autenticar(email,senha);
+		
+		//verificacao
+		Assertions.assertThat(result).isNotNull();
 	}
 	
 	@Test(expected = Test.None.class)//verificacao
